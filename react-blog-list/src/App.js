0346d001bd252f42328b9bf83167671bb93f3461
VladanStar <vladancupric@gmail.com>
2021-08-23 21:10:48 +0200

    React Blog List Done All

--- react-blog-list/src/App.js
+++ react-blog-list/src/App.js
@@ -1,7 +1,6 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Header from "./components/Header/Header";
-import BlogPost from "./components/BlogPost/BlogPost";
 import BlogList from "./components/BlogList/BlogList";
 
 function App() {
