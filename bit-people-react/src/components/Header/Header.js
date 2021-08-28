6c275624ff7ba376f6d9ec45836e7c87ad9f1659
unknown <vladancupric@gmail.com>
2021-08-28 12:57:50 +0200

    New commits

--- bit-people-react/src/components/Header/Header.js
+++ bit-people-react/src/components/Header/Header.js
@@ -4,12 +4,12 @@ import { IoMdRefresh } from 'react-icons/io';
 import { BsFillGrid3X3GapFill } from 'react-icons/bs';
 import { FaThList } from 'react-icons/fa';
 
+
 const Header = (props) => {
     return (
         <div className="Header__wrapper">
             <h1>BIT People</h1>
-            <div className="Header__switcher">
-                <h4>About</h4>
+            <div className="Header__switcher"> 
                 <IoMdRefresh onClick={props.reload} />
                 {props.switcher ? <BsFillGrid3X3GapFill
                     onClick={props.switchFunc} /> : <FaThList onClick={props.switchFunc} />}
